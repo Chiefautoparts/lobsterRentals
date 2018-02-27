cc69dd5801b9513cc14aa5caf861d1fa5c4a63ce
Chiefautoparts <chiefautoparts@outlook.com>
2018-02-27 01:08:59 -0800

    models built

--- lobyRentals_and_sales/shop/models.py
+++ lobyRentals_and_sales/shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	item = models.CharField(max_length=250)
+	slug = models.SlugField(max_length=250)
+	description = models.TextField()
+	image = models.ImageField()
+	availablilty = models.BooleanField(default=True)
+	stock = models.IntegerField()
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+	title = models.CharField(max_length=100)
