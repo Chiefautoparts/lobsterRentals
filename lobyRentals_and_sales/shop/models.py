49ffca4c33ce87a4cfc0cade0a4d1bc586fd89d8
Chiefautoparts <chiefautoparts@outlook.com>
2018-02-27 05:39:36 -0800

    did some shit

--- lobyRentals_and_sales/shop/models.py
+++ lobyRentals_and_sales/shop/models.py
@@ -8,8 +8,11 @@ class Product(models.Model):
 	image = models.ImageField()
 	availablilty = models.BooleanField(default=True)
 	stock = models.IntegerField()
+	category = models.ForeignKey(Category, related_name="Category")
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 
 class Category(models.Model):
 	title = models.CharField(max_length=100)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
